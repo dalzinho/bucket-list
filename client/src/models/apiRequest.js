72eb5bd8c692e94f3baf0ef3964df2469857e73a
Alastair Hood <alastair.hood85@gmail.com>
2017-04-05 11:28:46 +0100

    testing

---
+++ client/src/models/apiRequest.js
@@ -0,0 +1,20 @@
+var makeRequest = function(url, callback) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+
+  request.onload = callback;
+
+request.send();
+
+
+};
+
+  module.exports = makeRequest;
+
+
+
+
+
+
+
+
